d13aed4512a3db3cf987610f09fc164136429fc6
negin <negin.s.mousavi@gmail.com>
2021-11-07 16:17:12 +0330

    add TeacherService to write methods for teachers

---
+++ src/tamrin3/main/TeacherService.java
@@ -0,0 +1,20 @@
+package tamrin3.main;
+
+import tamrin3.Teacher;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Negin Mousavi
+ */
+public class TeacherService {
+    List<Teacher> teachers = new ArrayList<>();
+
+    public void addNewTeacher(Teacher teacher) {
+        teachers.add(teacher);
+    }
+
+//    public Optional<Teacher>
+}
